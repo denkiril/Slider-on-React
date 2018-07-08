9f234e6193bae9675c9e672e54a66aea8884ee89
Denis <den-kiril@yandex.ru>
2018-07-08 14:45:31 +0300

    Day 2 commit 2

--- src/App.js
+++ src/App.js
@@ -3,38 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+
 import styled from 'styled-components';
 
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-          <HelloW />
-          <SayFullName name="Denis" surname="Kiriliuk" link="https://vk.com/denkiril" />
-          <SayFullName name="Name" surname="Surname" link="#" />
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
       </div>
     );
   }
-}
-
-function HelloW() {
-  return (
-      <div>
-      <h1>Hello World</h1>
-      </div>
-  )
-}
-
-function SayFullName(props) {
-  return (
-      <div>
-      <h3> Моё имя {props.name}, фамилия - {props.surname}</h3>
-      <a href={props.link}> Ссылка на мой профиль </a>
-      </div>
-  )
 }
 
 
